2660b6b8c781c7a088c79258d1ef53f8958ed257
Administrator <jijason1991@gmail.com>
2021-12-03 22:33:24 +0800

    入金通知页面

--- frontend/controllers/FundController.php
+++ frontend/controllers/FundController.php
@@ -36,12 +36,14 @@ class FundController extends BaseController
             $account = $candidate['tradingAccount'];
         }
         $isNew = 1;
-        if (Yii::$app->request->isPost && $deposit->load(Yii::$app->request->post())) {
-            if ($deposit->validate()) {
-                $deposit->uid = $uid;
-                $deposit->save(false);
+        $saveModel = new Deposit();
+        if (Yii::$app->request->isPost && $saveModel->load(Yii::$app->request->post())) {
+            if ($saveModel->validate()) {
+                $saveModel->uid = $uid;
+                $saveModel->save(false);
                 $isNew = 0;
             }
+            $deposit = $saveModel;
         }
         return $this->render('index', [
             'account' => $account,
